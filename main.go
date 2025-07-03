b668dc6f774c1a71ec8fc52f7ec63e72a178b3a7
gucchunchun <yunataniguchi23@gmail.com>
2025-07-03 15:58:50 +0900

    2. 変数の宣言と初期化（var, :=）を使い分ける

--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import "fmt"
 
 // Entry point
 func main() {
-	// 課題１
-	fmt.Print("Hello World")
+	fmt.Println("## 課題１")
+	fmt.Println("Hello World")
+	fmt.Println()
+
+	fmt.Println("## 課題2")
+	// ○: initialize the zero-value of the type
+	var str_variable1 string
+	// △: not best practice?
+	var str_variable2 string = "Hello World1"
+	// ○: initialize the actual value
+	str_variable3 := "Hello World2"
+
+	str_variable1 = "Hello World3"
+	fmt.Println(str_variable1)
+	fmt.Println(str_variable2)
+	fmt.Println(str_variable3)
+	fmt.Println()
 }
